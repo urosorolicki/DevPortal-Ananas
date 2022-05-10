2aa5fc08eb4c1d5f03414665106e545fb6a12d7c
Uroš Orolicki <96778657+urosorolicki@users.noreply.github.com>
2022-05-10 13:32:13 +0200

    Edit

--- forms/contact.php
+++ forms/contact.php
@@ -1,52 +1,37 @@
 <?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'orolickiiuros@gmail.com';
 
-  if( file_exists($php_email_form = '...forms/validate.js' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
-
   $contact->ajax = true;
-  $email_to =   'orolickiiuros@gmail.com'; 
-$name     =   $_POST['name'];  
-$email    =   $_POST['email'];
-$subject  =   $_POST['subject'];
-$message  =   $_POST['message']. "\n\n";
-$headers  = "From: $email\r\n";
-$headers .= "Reply-To: $email\r\n";
-
-if(mail($email_to, $subject, $message, $headers)){
-    echo 'sent'; //       
-}else{
-    echo 'failed';// 
-}
-
-
-
   
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
   $contact->smtp = array(
-    'host' => 'http://127.0.0.1:5500/index3.html',
+    'host' => 'gmail.com',
     'username' => 'orolickiiuros@gmail.com',
     'password' => 'rusrxvpsjrpwwyfm',
-    'port' => '25'
+    'port' => '587'
   );
-  
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
-  echo '<script>window.location.href="index2.html";</script>';
-  
-
-
-
-
-
-
-
-
-
-
+?>
